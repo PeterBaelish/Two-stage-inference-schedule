0efe189af21fdd17e070e0078d581989ce5c0183
PeterBaelish <53077388+PeterBaelish@users.noreply.github.com>
2023-11-29 20:57:31 +0800

    Update decode.py

--- decode.py
+++ decode.py
@@ -14,15 +14,13 @@ def continue_text_with_kv_cache(input_texts, kv_caches, model_name='gpt2', max_l
     generated_texts = [None] * len(input_texts)
     streams = [Stream(device=device) for _ in range(2)]
 
-    first_kv_caches = [kv_caches[i].to(device) for i in range(min(batch_size, len(ordered_sentences)))] if ordered_sentences else []
-
+    current_kv_caches = [kv_caches[i].to(device) for i in range(min(batch_size, len(ordered_sentences)))] if ordered_sentences else []
+    is_first = True
+    
     while ordered_sentences:
         batch_len = min(batch_size, len(ordered_sentences))
         current_indices = [ordered_sentences[i][2] for i in range(batch_len)]
         current_batch = [ordered_sentences[i][1] for i in range(batch_len)]
-
-        next_batch_indices = [ordered_sentences[i + batch_size][2] for i in range(min(batch_size, len(ordered_sentences) - batch_size))]
-        next_kv_caches = [kv_caches[i].to(device) for i in next_batch_indices]
 
         input_ids = torch.cat([tokenizer.encode(text, return_tensors='pt') for text in current_batch], dim=0).to(device)
 
@@ -30,30 +28,38 @@ def continue_text_with_kv_cache(input_texts, kv_caches, model_name='gpt2', max_l
         adjusted_max_length = max_length if len(ordered_sentences) >= batch_size else None
 
         with torch.cuda.stream(streams[0]):
-            model_output = model.generate(input_ids, max_length=adjusted_max_length, pad_token_id=tokenizer.eos_token_id,
-                                          use_cache=True, return_dict_in_generate=True, past_key_values=first_kv_caches)
+            current_model_output = model.generate(input_ids, max_length=adjusted_max_length, pad_token_id=tokenizer.eos_token_id,
+                                          use_cache=True, return_dict_in_generate=True, past_key_values=current_kv_caches)
 
-        with torch.cuda.stream(streams[1]):
-            if current_indices[0] != 0:
+        with torch.cuda.stream(streams[1]): 
+            if is_first == False:
+                prev_generated_outputs = prev_model_output['sequences']
+                prev_updated_kv_caches = prev_model_output['past_key_values']
+                
                 for i, idx in enumerate(prev_batch_indices):
                     output = prev_generated_outputs[i].to('cpu')
                     text = tokenizer.decode(output, skip_special_tokens=True)
                     generated_texts[idx] = text
-                    if len(text) < max_length and not text.endswith('.'):
+                    if not text.endswith('.'): # maybe not endwith EOS
                         ordered_sentences.append((len(text), text, idx))
                         kv_caches[idx] = prev_updated_kv_caches[i].to('cpu')
-
-            ordered_sentences = ordered_sentences[batch_len:]
-            ordered_sentences.sort(key=lambda x: x[0])
-
-            prev_batch_indices = current_indices
-            prev_generated_outputs = model_output['sequences']
-            prev_updated_kv_caches = model_output['past_key_values']
-
-            first_kv_caches = next_kv_caches
+                
+                del prev_model_output
+                
+                ordered_sentences.sort(key=lambda x: x[0])
+            else:
+                is_first = False
+            
+            next_batch_indices = [ordered_sentences[i + batch_size][2] for i in range(min(batch_size, len(ordered_sentences) - batch_size))]
+            ordered_sentences = [ordered_sentences[i] for i not in next_batch_indices]
+            next_kv_caches = [kv_caches[i].to(device) for i in next_batch_indices]
+            current_kv_caches = next_kv_caches
 
         streams[1].synchronize()
         streams[2].synchronize()
+
+        prev_batch_indices = current_indices
+        prev_model_output = current_model_output
 
     return [text for text in generated_texts if text is not None], kv_caches
 
